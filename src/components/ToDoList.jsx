826d7fd33e30be7ca13dcd5a6fdcb665132f93d6
katarighe <m.ighe@hotmail.com>
2023-08-14 14:44:03 +0300

    Rewrite the title

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -45,7 +45,7 @@ function ToDoList() {
 
   return (
     <div>
-      <h1>What are your current To Do tasks for today?</h1>
+      <h1>What tasks do you have on your to-do list for today?</h1>
       <ToDoForm onSubmit={addTodo} clearTodo={clearTodo} />
       <ToDoCount todos={todos} />
       <ToDo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
