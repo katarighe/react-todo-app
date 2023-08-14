48cb933b763b4c04b7cff20bfdd04796fe1ec8e4
katarighe <m.ighe@hotmail.com>
2023-08-14 13:00:47 +0300

    Add the function that counts the list of todo tasks

---
+++ src/components/ToDoCount.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ToDoCount({ todos }) {
+    return (
+        <div className="todo-count">
+            You have
+            {!todos.length 
+            ? "no todo tasks"
+            : todos.length === 1
+            ? "1 todo task"
+            : todos.length > 1
+            ? ` ${todos.length} todos`
+            : null }
+            </div>
+    );
+}
+
+export default ToDoCount;
