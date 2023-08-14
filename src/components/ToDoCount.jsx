1aecab27d3644187c040b66ac5fb6355b9b6ede9
katarighe <m.ighe@hotmail.com>
2023-08-14 15:24:52 +0300

    Fix sentence spacing

--- src/components/ToDoCount.jsx
+++ src/components/ToDoCount.jsx
@@ -5,9 +5,9 @@ function ToDoCount({ todos }) {
     <div className="to-do-count">
       There is
       {!todos.length
-        ? "no todo tasks that you need to complete."
+        ? " no todo tasks that you need to complete. "
         : todos.length === 1
-        ? "1 todo task that you need to complete."
+        ? " 1 todo task that you need to complete. "
         : todos.length > 1
         ? ` ${todos.length} todo tasks that you need to complete.`
         : null}
