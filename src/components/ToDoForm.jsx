56bc7edfd4bd70758669ad728a13c358906f9649
katarighe <m.ighe@hotmail.com>
2023-08-14 13:08:53 +0300

    Formatt the spaces and lines

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -3,52 +3,52 @@ import { AiOutlineClear } from "react-icons/ai";
 import { GrFormAdd } from "react-icons/gr";
 
 function ToDoForm(props) {
-    const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
 
-    const inputRef = useRef(null);
+  const inputRef = useRef(null);
 
-    useEffect(() => {
-        inputRef.current.focus();
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: new Date().getTime(),
+      text: input,
     });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    setInput("");
+  };
 
-        props.onSubmit({
-            id: new Date().getTime(),
-            text: input,
-        });
+  const handleClear = (e) => {
+    e.preventDefault();
 
-        setInput("");
-    };
+    props.clearTodo();
 
-    const handleClear = (e) => {
-        e.preventDefault();
+    setInput("");
+  };
 
-        props.clearTodo();
-
-        setInput("");
-    };
-
-    return (
-        <form className="todo-form" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="text"
-            value={input}
-            placeholder="Add todo"
-            className="todo-input"
-            onChange={(e) => setInput(e.target.value)}
-            ref={inputRef}
-          />
-          <button className="add-button">
-            <GrFormAdd className="add-icon" />
-          </button>
-          <button className="clear-button" onClick={handleClear}>
-            <AiOutlineClear className="clear-icon" />
-          </button>
-        </form>
-    );
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="text"
+        value={input}
+        placeholder="Add todo"
+        className="todo-input"
+        onChange={(e) => setInput(e.target.value)}
+        ref={inputRef}
+      />
+      <button className="add-button">
+        <GrFormAdd className="add-icon" />
+      </button>
+      <button className="clear-button" onClick={handleClear}>
+        <AiOutlineClear className="clear-icon" />
+      </button>
+    </form>
+  );
 }
 
 export default ToDoForm;
