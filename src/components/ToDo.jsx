0d0b6c780034ad91de734dea241f0330857d56fc
katarighe <m.ighe@hotmail.com>
2023-08-14 15:47:29 +0300

    Fix errors relating to the delete function

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { RiCloseCircleLine } from 'react-icons/ri';
 
-function ToDo({ todos, completeToDo, removeToDo }) {
+function ToDo({ todos, completeTodo, removeTodo }) {
   return todos.map((todo, index) => (
     <div
       className={todo.isComplete ? 'to-do-row complete' : 'to-do-row'}
       key={index}
     >
-      <div key={todo.id} onClick={() => completeToDo(todo.id)}>
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
         {todo.text}
       </div>
       <RiCloseCircleLine
-        onClick={() => removeToDo(todo.id)}
+        onClick={() => removeTodo(todo.id)}
         className='delete-icon'
       />
     </div>
