94469d1f81188020bd5070d75e0cea612370d5e5
katarighe <m.ighe@hotmail.com>
2023-08-14 12:59:54 +0300

    Fix formatting and spacing lines

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,19 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 
 function ToDo({ todos, completeTodo, removeTodo }) {
-    return todos.map((todo, index) => (
-        <div className={todo.isComplete ? "todo-row complete" : "todo-row"}
-        key={index}
-        >
-            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-                {todo.text}
-            </div>
-            <RiCloseCircleLine onClick={() => removeTodo(todo.id)}
-            className="delete-icon"
-            />
-        </div>
-    ));
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <RiCloseCircleLine
+        onClick={() => removeTodo(todo.id)}
+        className="delete-icon"
+      />
+    </div>
+  ));
 }
 
 export default Todo;
