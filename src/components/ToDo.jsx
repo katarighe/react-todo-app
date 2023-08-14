50d122519785fe8e2829e579a49411390f5bb489
katarighe <m.ighe@hotmail.com>
2023-08-14 13:10:29 +0300

    Fix syntaxes

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 
-function ToDo({ todos, completeTodo, removeTodo }) {
+function ToDo({ todos, completeToDo, removeToDo }) {
   return todos.map((todo, index) => (
     <div
       className={todo.isComplete ? "todo-row complete" : "todo-row"}
       key={index}
     >
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+      <div key={todo.id} onClick={() => completeToDo(todo.id)}>
         {todo.text}
       </div>
       <RiCloseCircleLine
-        onClick={() => removeTodo(todo.id)}
+        onClick={() => removeToDo(todo.id)}
         className="delete-icon"
       />
     </div>
   ));
 }
 
-export default Todo;
+export default ToDo;
